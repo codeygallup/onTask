647cbb1357b2ac6ddfd8a6bcbac0afd460347282
Codeyg12 <codey.gallup@gmail.com>
2024-04-22 14:18:06 -0600

    front page styling

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -4,38 +4,23 @@ import { USER_PROJECTS } from "../utils/queries";
 import Auth from "../utils/auth";
 
 const Home = () => {
+  const { loading, data } = useQuery(USER_PROJECTS);
+  const userArr = data?.userProjects || [];
 
-  const { loading, data } = useQuery(USER_PROJECTS)
-  const userArr = data?.userProjects || []
- 
   return (
     <div className="container-fluid w-95 border text-center">
       <div className="card-header bg-dark">
         <h1>Welcome to OnTask App!</h1>
         {Auth.loggedIn() ? (
-          <button className="btn bg-dark logout" onClick={Auth.logout}>Logout</button>
-        ) : null }
+          <button className="btn bg-dark logout" onClick={Auth.logout}>
+            Logout
+          </button>
+        ) : null}
       </div>
-      <div className="card-body">
-        <h2 className="mt-4">Project List</h2>
-         {loading ? (
-          <div>Loading...</div>
-        ) : (
-          <div className="text-center">
-            {userArr.map((project) => {
-              return (
-                <p className={userArr.length < 10 ? "projectList" : "projectLongList"} key={project._id}>
-                  <Link to={`/project/${project._id}`}>{project.title}</Link>
-                </p>
-              );
-            })}
-          </div>
-        )}
-      </div>
-      <div className="card-footer text-center m-3">
-        <h2>Ready to create a new project?</h2>
+      <div className="card-body text-center m-3">
         {!Auth.loggedIn() ? (
           <>
+          <h2>Create an account or sign in to view projects</h2>
             <Link className="mx-4" to="/login">
               <button className="btn btn-lg btn-dark">Login</button>
             </Link>
@@ -44,9 +29,30 @@ const Home = () => {
             </Link>
           </>
         ) : (
-          <Link to="/project">
-            <button className="btn btn-dark">Add Project</button>
-          </Link>
+          <>
+            <div className="card-body p-5">
+              <div className="text-center">
+                {userArr.map((project) => {
+                  return (
+                    <p
+                      className={
+                        userArr.length < 10 ? "projectList" : "projectLongList"
+                      }
+                      key={project._id}
+                    >
+                      <Link to={`/project/${project._id}`}>
+                        {project.title}
+                      </Link>
+                    </p>
+                  );
+                })}
+              </div>
+            </div>
+            <h2 className="mt-4">Project List</h2>
+            <Link to="/project">
+              <button className="btn btn-dark">Add Project</button>
+            </Link>
+          </>
         )}
       </div>
     </div>
