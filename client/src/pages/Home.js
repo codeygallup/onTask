4c577f97e5c5527929e8b1cd6c011a444c7cf640
Codeyg12 <codey.gallup@gmail.com>
2024-04-27 17:36:58 -0600

    added plain bootstrap

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -8,55 +8,74 @@ const Home = () => {
   const userArr = data?.userProjects || [];
 
   return (
-    <div className="container-fluid w-95 border text-center">
-      <div className="card-header bg-dark">
-        <h1>Welcome to OnTask App!</h1>
-        {Auth.loggedIn() ? (
-          <button className="btn bg-dark logout" onClick={Auth.logout}>
-            Logout
-          </button>
-        ) : null}
+    <>
+      <div className="d-flex justify-content-center align-items-center vh-100 mx-3">
+        <div className="card container-fluid border text-center shadow p-2">
+          <div className="card-header d-flex align-items-center justify-content-between">
+            <h1 className="card-title">Welcome to OnTask!</h1>
+            {
+            Auth.loggedIn() && (
+              <button className="btn bg-dark text-white" onClick={Auth.logout}>
+                Logout
+              </button>
+            )}
+          </div>
+          <div className="card-body my-4">
+            {!Auth.loggedIn() ? (
+              <>
+                <h2 className="mb-5">Create an account or sign in to view projects</h2>
+                <Link className="mx-4" to="/login">
+                  <button className="btn btn-lg btn-dark">Login</button>
+                </Link>
+                <Link className="mx-4" to="/signup">
+                  <button className="btn btn-lg btn-dark">Signup</button>
+                </Link>
+              </>
+            ) : (
+              <></>
+            )}
+          </div>
+        </div>
       </div>
-      <div className="card-body text-center m-3">
-        {!Auth.loggedIn() ? (
-          <>
-          <h2>Create an account or sign in to view projects</h2>
-            <Link className="mx-4" to="/login">
-              <button className="btn btn-lg btn-dark">Login</button>
-            </Link>
-            <Link className="mx-4" to="/signup">
-              <button className="btn btn-lg btn-dark">Signup</button>
-            </Link>
-          </>
-        ) : (
-          <>
-            <div className="card-body p-5">
-              <div className="text-center">
-                {userArr.map((project) => {
-                  return (
-                    <p
-                      className={
-                        userArr.length < 10 ? "projectList" : "projectLongList"
-                      }
-                      key={project._id}
-                    >
-                      <Link to={`/project/${project._id}`}>
-                        {project.title}
-                      </Link>
-                    </p>
-                  );
-                })}
-              </div>
-            </div>
-            <h2 className="mt-4">Project List</h2>
-            <Link to="/project">
-              <button className="btn btn-dark">Add Project</button>
-            </Link>
-          </>
-        )}
-      </div>
-    </div>
+    </>
   );
 };
 
 export default Home;
+
+// {
+{
+  /* <div className="card-body text-center m-3">
+  {!Auth.loggedIn() ? (
+    <>
+    </>
+  ) : (
+    <>
+      <div className="card-body p-5">
+        <div className="text-center">
+          {userArr.map((project) => {
+            return (
+              <p
+                className={
+                  userArr.length < 10 ? "projectList" : "projectLongList"
+                }
+                key={project._id}
+              >
+                <Link to={`/project/${project._id}`}>
+                  {project.title}
+                </Link>
+              </p>
+            );
+          })}
+        </div>
+      </div>
+      <h2 className="mt-4">Project List</h2>
+      <Link to="/project">
+        <button className="btn btn-dark">Add Project</button>
+      </Link>
+    </>
+  )}
+</div>
+</div>  */
+}
+// }
