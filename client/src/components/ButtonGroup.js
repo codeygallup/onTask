ebfa6146bc0b503d23d1d07e7eb90b02950edc86
Codeyg12 <codey.gallup@gmail.com>
2024-04-29 17:33:06 -0600

    Created a new component to hold all logic for the buttons

---
+++ client/src/components/ButtonGroup.js
@@ -0,0 +1,73 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faArrowRotateLeft,
+  faCheck,
+  faCheckDouble,
+  faXmark,
+} from "@fortawesome/free-solid-svg-icons";
+import { useContext } from "react";
+import { TaskContext } from "./TaskContext";
+
+export default function ButtonGroup({ task }) {
+  let { removeTask, updateComplete, refetch } = useContext(TaskContext);
+  const taskDelete = async (e, taskId) => {
+    e.preventDefault();
+
+    try {
+      await removeTask({
+        variables: { taskId },
+      });
+      refetch();
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  const changeStatus = async (e, taskId) => {
+    e.preventDefault();
+
+    try {
+      await updateComplete({
+        variables: { taskId },
+      });
+      refetch();
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  return (
+    <>
+      {!task.complete ? (
+        <button
+          className="btn btn-danger"
+          onClick={(e) => {
+            changeStatus(e, task._id);
+          }}
+        >
+          <FontAwesomeIcon icon={faCheck} />
+        </button>
+      ) : (
+        <div className="btn-group">
+          <button
+            className="btn btn-success"
+            onClick={(e) => {
+              changeStatus(e, task._id);
+            }}
+          >
+            <FontAwesomeIcon icon={faArrowRotateLeft} />
+          </button>
+          <button
+            className="btn btn-danger"
+            onClick={(e) => {
+              taskDelete(e, task._id);
+            }}
+          >
+            {/* <FontAwesomeIcon icon={faCheckDouble} /> */}
+            <FontAwesomeIcon icon={faXmark} />
+          </button>
+        </div>
+      )}
+    </>
+  );
+}
--- client/src/components/TaskItem.js
+++ client/src/components/TaskItem.js
@@ -1,63 +1,13 @@
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faCheck, faXmark } from "@fortawesome/free-solid-svg-icons";
-import { useContext } from "react";
-import { TaskContext } from "./TaskContext";
+import ButtonGroup from "./ButtonGroup";
 
 export default function TaskItem({ task }) {
-
-  let { removeTask, updateComplete, refetch } = useContext(TaskContext);
-  const taskDelete = async (e, taskId) => {
-    e.preventDefault();
-
-    try {
-      await removeTask({
-        variables: { taskId },
-      });
-      refetch();
-    } catch (err) {
-      console.log(err);
-    }
-  };
-
-  const changeStatus = async (e, taskId) => {
-    e.preventDefault();
-
-    try {
-      await updateComplete({
-        variables: { taskId },
-      });
-      refetch();
-    } catch (err) {
-      console.log(err);
-    }
-  };
-
   return (
     <>
       <div className="d-flex justify-content-between align-items-center my-2">
-        <p
-          className={`${
-            task.complete ? "task-complete" : ""
-          }`}
-        >
+        <p className={`${task.complete ? "task-complete" : ""}`}>
           {task.taskText}
         </p>
-        <button
-          className="btn btn-danger"
-          onClick={(e) => {
-            taskDelete(e, task._id);
-          }}
-        >
-          <FontAwesomeIcon icon={faXmark} />
-        </button>
-        <button
-          className="btn btn-danger"
-          onClick={(e) => {
-            changeStatus(e, task._id);
-          }}
-        >
-          <FontAwesomeIcon icon={faCheck} />
-        </button>
+        <ButtonGroup task={task} />
       </div>
     </>
   );
