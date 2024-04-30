3dfae2d66cd4c6a8de54dbbd02f7173f67fad745
Codeyg12 <codey.gallup@gmail.com>
2024-04-30 13:03:31 -0600

    adding protection for routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,25 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { ApolloClient, ApolloProvider, InMemoryCache, createHttpLink } from "@apollo/client";
-import './App.css'
-import { setContext } from '@apollo/client/link/context'
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+  Outlet,
+  Navigate,
+} from "react-router-dom";
+import {
+  ApolloClient,
+  ApolloProvider,
+  InMemoryCache,
+  createHttpLink,
+} from "@apollo/client";
+import "./App.css";
+import { setContext } from "@apollo/client/link/context";
 
 import Home from "./pages/Home";
 import Login from "./pages/Login";
 import SignUp from "./pages/Signup";
-import Project from './pages/Project';
-import ProjectPage from './pages/ProjectPage'
-import ProjectUpdate from './pages/ProjectUpdate'
+import Project from "./pages/Project";
+import ProjectPage from "./pages/ProjectPage";
+import ProjectUpdate from "./pages/ProjectUpdate";
 
 const http = createHttpLink({
   uri: "/graphql",
@@ -22,12 +33,18 @@ const authLink = setContext((_, { headers }) => {
       authorization: token ? `Bearer ${token}` : "",
     },
   };
-})
+});
 
 const client = new ApolloClient({
   link: authLink.concat(http),
   cache: new InMemoryCache(),
 });
+
+const PrivateRoutes = () => {
+  const isAuthenticated = !!localStorage.getItem("id_token");
+
+  return isAuthenticated ? <Outlet /> : <Navigate to={"/"} />;
+};
 
 function App() {
   return (
@@ -38,9 +55,11 @@ function App() {
             <Route path="/" element={<Home />} />
             <Route path="/signup" element={<SignUp />} />
             <Route path="/login" element={<Login />} />
-            <Route path="/project" element={<Project />} />
-            <Route path="/project/:id" element={<ProjectPage />} />
-            <Route path="/project/:id/update" element={<ProjectUpdate />} />
+            <Route element={<PrivateRoutes />}>
+              <Route path="/project" element={<Project />} />
+              <Route path="/project/:id" element={<ProjectPage />} />
+              <Route path="/project/:id/update" element={<ProjectUpdate />} />
+            </Route>
           </Routes>
         </div>
       </BrowserRouter>
