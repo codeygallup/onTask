5c219b0af0ad0115d9fc8abe81f45f84f01746cd
Codeyg12 <codey.gallup@gmail.com>
2024-05-08 18:06:46 -0600

    nodemailer email

--- server/utils/passwordRecovery.js
+++ server/utils/passwordRecovery.js
@@ -1,3 +1,28 @@
-// https://nodemailer.com/about/
+const nodemailer = require("nodemailer");
+require('dotenv').config()
 
-// https://stackoverflow.com/questions/61727215/react-reset-password-send-email-with-reset-password-link-to-reset-the-password
+const transporter = nodemailer.createTransport({
+  host: "smtp-mail.outlook.com",
+  port: 587,
+  secure: false, // Use `true` for port 465, `false` for all other ports
+  auth: {
+    user: process.env.EMAIL,
+    pass: process.env.PASS,
+  },
+});
+
+async function passwordRecover(to) {
+    console.log(to);
+  // send mail with defined transport object
+  const info = await transporter.sendMail({
+    from: '"OnTask 👻" <codey.gallup@hotmail.com>', // sender address
+    to: to, // list of receivers
+    subject: "Hello ✔", // Subject line
+    text: "Hello world?", // plain text body
+    html: "<b>Hello world?</b>", // html body
+  });
+
+  console.log("Message sent: %s", info.messageId);
+}
+
+module.exports = passwordRecover;
