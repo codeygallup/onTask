187658a4cf2131af9a060fba6b54695d51055b3b
Codeyg12 <codey.gallup@gmail.com>
2025-08-03 20:12:58 -0600

    Minor clean up and updating package json to deploy

--- server/server.js
+++ server/server.js
@@ -22,14 +22,14 @@ if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../client/build")));
 }
 
-app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "../client/build/index.html"));
-});
-
 const startApolloServer = async () => {
   try {
     await server.start();
     server.applyMiddleware({ app });
+
+    app.get("*", (req, res) => {
+      res.sendFile(path.join(__dirname, "../client/build/index.html"));
+    });
 
     db.once("open", () => {
       app.listen(PORT, () => {
