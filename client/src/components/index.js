843d5324e466ba32b5252a334af935017a595080
Codeyg12 <codey.gallup@gmail.com>
2025-11-20 17:19:57 -0700

    Removing HomeButton

--- client/src/components/index.js
+++ client/src/components/index.js
@@ -1,8 +1,7 @@
-import HomeButton from "./HomeButton";
 import CardHeader from "./CardHeader";
 import LoginForm from "./LoginForm";
 import ProjectForm from "./ProjectForm";
 import TaskInput from "./TaskInput";
 import TaskItem from "./TaskItem";
 
-export { HomeButton, CardHeader, LoginForm, ProjectForm, TaskInput, TaskItem };
+export { CardHeader, LoginForm, ProjectForm, TaskInput, TaskItem };
